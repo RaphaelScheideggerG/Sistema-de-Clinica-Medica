e445d2dede5d8becd04f2b85cae7b165dc83e9a2
matheusgg18 <math147969@gmail.com>
2025-11-26 09:15:26 -0300

    teste

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [
     react({
-      babel: {
+      babel: { // oi oi
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
