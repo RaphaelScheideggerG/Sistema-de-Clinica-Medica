94bd3adcfdf33f6a3752e83ebd3e0c7e9eb40c4e
AnaCarolinaMoraesBelo <moraesbeloa@gmail.com>
2025-11-30 10:52:21 -0300

    Adicionando a PacienteForm

--- src/componentes/cadastrapessoas/PacienteForm.jsx
+++ src/componentes/cadastrapessoas/PacienteForm.jsx
@@ -1,17 +1,24 @@
+import React from "react";
 import { Form, Input, DatePicker } from "antd";
 
-export default function PFForm() {
+export default function PacienteForm() {
   return (
     <>
       <Form.Item
         label="CPF"
         name="cpf"
-        rules={[{ required: true, message: "Informe o CPF!" }]}
+        rules={[
+          { required: true, message: "Informe o CPF!" },
+          { len: 11, message: "O CPF deve ter 11 números exatos" }
+        ]}
       >
         <Input placeholder="Somente números" maxLength={11} />
       </Form.Item>
 
       <Form.Item label="Data de Nascimento" name="dataNascimento">
+      rules={[
+        { required: true, message: "Informe a data de nascimento!" }
+        ]} 
         <DatePicker format="DD/MM/YYYY" style={{ width: "100%" }} />
       </Form.Item>
     </>
