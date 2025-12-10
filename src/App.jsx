063bdf54a44478ec7a95964a7a2f01360b6bc8c8
AnaCarolinaMoraesBelo <moraesbeloa@gmail.com>
2025-12-09 22:52:44 -0300

    modificando temporariamente app para teste

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import AppRoutes from "./routes/AppRoutes";
-
+import FormularioConsulta from "./componentes/formularioconsulta/FormularioConsulta"; 
 
 function App() {
-  return <AppRoutes />;
-}
+  return (
+    <div>
+      {
+        
+      }
+      <h2>Teste do Formulário:</h2>
+      <FormularioConsulta />
+      <hr /> {
 
+      }
+
+      <AppRoutes />
+    </div>
+  );
+}
 export default App;
